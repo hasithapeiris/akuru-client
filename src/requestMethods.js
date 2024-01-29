6ad91632f2c27af3554cb24286f0cda86752aea4
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-01-29 14:48:48 +0530

    update server URL

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://akuru-v2-734ecd434119.herokuapp.com/api/";
+const BASE_URL = "https://akuru-server.onrender.com/api/";
 // const BASE_URL = "http://localhost:5000/api/";
 
 export const publicRequest = axios.create({
